4baba30c460cd35715ccfde691d944d0ddd10e88
Monique Jackson <ST10096757@vcconnect.edu.za>
2024-11-16 10:33:52 +0200

    Added Newtonsoft.json NuGet Package & updates ServiceRequest model (Taking a new approach that doesn't feel stupid)

--- ST10096757_MoniqueJackson_MunicipalityApp_Part2/Models/ServiceRequest.cs
+++ ST10096757_MoniqueJackson_MunicipalityApp_Part2/Models/ServiceRequest.cs
@@ -6,21 +6,21 @@ using System.Threading.Tasks;
 
 namespace ST10096757_MoniqueJackson_MunicipalityApp_Part2.Models
 {
+	/// <summary>
+	/// Store data types that will be used for the POE part of the project
+	/// </summary>
 	public class ServiceRequest
 	{
-		public int RequestId { get; set; }
-		public string Description { get; set; }
-		public string Status { get; set; }
-		public string Priority { get; set; }
-		public DateTime DateSubmitted { get; set; }
-
-		public ServiceRequest(int id, string description, string status, string priority, DateTime date)
-		{
-			RequestId = id;
-			Description = description;
-			Status = status;
-			Priority = priority;
-			DateSubmitted = date;
-		}
+		public string RequestId { get; set; }             // Unique identifier for the request
+		public string ResidentName { get; set; }          // Name of the resident reporting the issue
+		public string ContactDetails { get; set; }        // Contact info of the resident
+		public string RequestType { get; set; }           // Type of service request (e.g., Water Leak, Road Repair)
+		public string Description { get; set; }           // Description of the issue/request
+		public DateTime SubmissionDate { get; set; }      // Date when the request was submitted
+		public string Status { get; set; }                // Status of the request (e.g., Pending, In Progress, Resolved)
+		public string Priority { get; set; }              // Priority (e.g., High, Medium, Low)
+		public string AssignedStaff { get; set; }         // Staff assigned to handle the request
+		public string Location { get; set; }              // Physical location of the issue
+		public List<string> ProgressLogs { get; set; }    // List to store updates about the request status
 	}
 }
