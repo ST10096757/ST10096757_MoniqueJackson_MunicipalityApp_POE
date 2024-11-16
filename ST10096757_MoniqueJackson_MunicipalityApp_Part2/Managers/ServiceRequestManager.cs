7dde4e222f82cfd113be95708a8247de75494d52
Monique Jackson <ST10096757@vcconnect.edu.za>
2024-11-16 10:46:25 +0200

    Added ServiceRequestManager

---
+++ ST10096757_MoniqueJackson_MunicipalityApp_Part2/Managers/ServiceRequestManager.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using ST10096757_MoniqueJackson_MunicipalityApp_Part2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10096757_MoniqueJackson_MunicipalityApp_Part2.Managers
+{
+	public class ServiceRequestManager
+	{
+		private const string FilePath = "service_requests.json";
+
+		// Method to serialize and save service requests to a file
+		public void SaveServiceRequests(Dictionary<string, ServiceRequest> serviceRequests)
+		{
+			try
+			{
+				// Convert the dictionary to a JSON string
+				string json = JsonConvert.SerializeObject(serviceRequests, Formatting.Indented);
+
+				// Write the JSON string to a file
+				File.WriteAllText(FilePath, json);
+				Console.WriteLine("Service requests saved successfully.");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"An error occurred while saving service requests: {ex.Message}");
+			}
+		}
+	}
+}
